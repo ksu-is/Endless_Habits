f559e090bc0e353df91d3dfedf895f9f57c861aa
Amiyah <amiyah.richardson312@gmail.com>
2022-12-05 21:08:51 -0500

    main python file free of bugs

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import pandas as pd
 from tabulate import tabulate
 
 habits =[
-    habit_break('coffee', datetime(2022, 11, 23, 10, 45))
+    habit_break('coffee', datetime(2022, 11, 23, 10, 45)),
+    habit_break('vaping', datetime(2022, 12, 2, 8, 16)),
+    habit_break('sleeping late', datetime(2022, 11, 15, 5, 28)),
 ]
 
 df = pd.DataFrame(habits)
 
-print(df)
+print(tabulate(df, headers='keys', tablefmt='psql'))
